9f2a4acb97294ebc21ee8b10bb303d3fd5e97af9
hackertron <cooljay.gupta@gmail.com>
2018-02-19 08:51:09 +0530

    a lot of changes

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
-
+var ejs = require('ejs');
+var engine = require('ejs-mate');
 var app = express();
 
 var secret = require('./config/secret');
@@ -16,6 +17,9 @@ mongoose.connect(secret.database, function(err){
   }
 });
 
+app.use(express.static(__dirname + '/public'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(morgan('dev'));
