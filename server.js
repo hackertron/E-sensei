a1fd81eb9519f8b914e4038dbc357ecb8ac207ee
hackertron <cooljay.gupta@gmail.com>
2018-02-19 07:53:11 +0530

    mongoose

--- server.js
+++ server.js
@@ -5,10 +5,26 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('', function(err){
+mongoose.connect('your url here ', function(err){
   if(err){
     console.log(err);
   } else {
     console.log("connected to DB ");
+  }
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(morgan('dev'));
+
+app.get('/', function(req, res, next){
+  res.json('home');
+})
+
+app.listen(8080, function(err){
+  if(err){
+    console.log(err);
+  } else {
+    console.log("listening on 8080 ");
   }
 });
