0b6b091847542333f5ba2256fc8918cc18f46468
hackertron <cooljay.gupta@gmail.com>
2018-02-21 22:14:46 +0530

    revenue

--- models/user.js
+++ models/user.js
@@ -2,19 +2,25 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  email: { type : String, unique: true, lowercase: true},
+  email: { type: String, unique: true, lowercase: true},
   facebook: String,
   tokens: Array,
   role: String,
   profile: {
     name: { type: String, default: ''},
-    picture: {type: String, default: ''}
+    picture: { type: String, default: ''}
   },
+
   coursesTeach: [{
-    course: {type: Schema.Types.ObjectId, ref: 'Course'}
+    course: { type: Schema.Types.ObjectId, ref: 'Course'}
   }],
+
   coursesTaken: [{
-    course: {type: Schema.Types.ObjectId, ref: 'Course'}
+    course: { type: Schema.Types.ObjectId, ref: 'Course'}
+  }],
+
+  revenue: [{
+    money: Number
   }]
 });
 
